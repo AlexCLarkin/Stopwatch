bc32f3cee92fe4e2cda4bb68e10237d457235eb3
AlexCLarkin <98946206+AlexCLarkin@users.noreply.github.com>
2022-03-24 08:52:22 +0000

    Add files via upload

--- stopwatch/css/stopwatch.js
+++ stopwatch/css/stopwatch.js
@@ -14,7 +14,7 @@ window.onload = function () {
       clearInterval(Interval);
        Interval = setInterval(startTimer, 10);
     }
-    
+          
       buttonStop.onclick = function() {
          clearInterval(Interval);
     }
@@ -55,7 +55,24 @@ window.onload = function () {
       }
     
     }
+
     
-    var dt = new Date();
-    document.getElementById("dateTime").innerHTML = dt.toLocaleTimeString();
+     var currentTime = new Date()
+     var hours = currentTime.getHours()
+     var minutes = currentTime.getMinutes()
+     var sec = currentTime.getSeconds()
+     if (minutes < 10){
+         minutes = "0" + minutes
+     }
+     if (sec < 10){
+         sec = "0" + sec
+     }
+     var t_str = hours + ":" + minutes + " ";
+     if(hours > 11){
+         t_str += "PM";
+     } else {
+        t_str += "AM";
+     }
+      document.getElementById('time_span').innerHTML = t_str;
+      setTimeout(time,1000);
   }
